289f7e0d0f881186df44ccefb4ecfcb5544fdd51
jskang24 <jskang24@pupils.nlcsjeju.kr>
2023-01-12 03:38:10 +0900

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/NavBar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Mission from "./pages/Mission";
+import Contact from "./pages/Contact";
+import Opportunities from "./pages/Opportunites";
+import OurProduct from "./pages/OurProduct";
+import Testimonials from "./pages/Testimonials";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact component={Home} />
+        <Route path="/mission" component={Mission} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/opportunities" component={Opportunities} />
+        <Route path="/product" component={OurProduct} />
+        <Route path="/testimonials" component={Testimonials} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/NavBar/Bar.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { FaBars } from "react-icons/fa";
+import "./Navbar.css";
+
+const Bar = () => {
+  return <FaBars className="Bar" />;
+};
+
+export default Bar;
