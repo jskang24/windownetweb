2ce942b38c288331561efeb06d2139200bfedb64
jskang24 <jskang24@pupils.nlcsjeju.kr>
2023-01-12 03:37:49 +0900

    Navbar

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,69 @@
+import React from "react";
+import "./Navbar.css";
+import { NavLink as Link } from "react-router-dom";
+import styled from "styled-components";
+
+import Bar from "./Bar";
+
+const NavLink = styled(Link)`
+  color: #ffffff;
+  display: flex;
+  align-items: center;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+  text-decoration: none;
+  font-family: "Gudea", sans-serif;
+  font-size: 20px;
+  &.active {
+    font-weight: "bold";
+  }
+`;
+
+const HomeLink = styled(Link)`
+  color: #ffffff;
+  display: flex;
+  align-items: center;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+  text-decoration: none;
+  font-family: "Gudea", sans-serif;
+  font-weight: bold;
+  font-size: 28px;
+  &.active {
+    font-weight: "bold";
+  }
+`;
+
+const Navbar = () => {
+  return (
+    <div className="Nav">
+      <Bar />
+      <div className="Title">
+        <HomeLink to="/" activeStyle>
+          Window Net
+        </HomeLink>
+      </div>
+      <div className="NavMenu">
+        <NavLink to="/mission" activeStyle>
+          Mission
+        </NavLink>
+        <NavLink to="/product" activeStyle>
+          Product
+        </NavLink>
+        <NavLink to="/testimonials" activeStyle>
+          Testimonials
+        </NavLink>
+        <NavLink to="/opportunities" activeStyle>
+          Opportunities
+        </NavLink>
+        <NavLink to="/contact" activeStyle>
+          Contact
+        </NavLink>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
