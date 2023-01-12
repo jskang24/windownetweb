afc5e5461d08c34d54b40a3cd886344fc681127b
jskang24 <jskang24@pupils.nlcsjeju.kr>
2023-01-13 01:17:08 +0900

    unused-code fix2

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Navbar.css";
-import { NavLink as Link, useLocation } from "react-router-dom";
+import { NavLink as Link } from "react-router-dom";
 import styled from "styled-components";
 
 import Bar from "./Bar";
