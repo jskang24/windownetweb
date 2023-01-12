444cd2627541e199a8fe7a4eccbfcd0b49360f0b
jskang24 <jskang24@pupils.nlcsjeju.kr>
2023-01-13 03:42:27 +0900

    Contact Animation

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -9,19 +9,21 @@ const Contact = () => {
   return (
     <div>
       <Navbar />
-      <div className="Contact_Slogan">
-        <div className="Contact_Empty" />
-        <span className="Contact_Text">
-          Close, Change, <br /> Save, and Sustain
-        </span>
+      <div className="Contact">
+        <div className="Contact_Slogan">
+          <div className="Contact_Empty" />
+          <span className="Contact_Text">
+            Close, Change, <br /> Save, and Sustain
+          </span>
+        </div>
+        <div className="Contact_Support">
+          <div className="Contact_Empty" />
+          <span className="Contact_Text">
+            Support At The <br /> Ready
+          </span>
+        </div>
+        <Footer />
       </div>
-      <div className="Contact_Support">
-        <div className="Contact_Empty" />
-        <span className="Contact_Text">
-          Support At The <br /> Ready
-        </span>
-      </div>
-      <Footer />
     </div>
   );
 };
