562a3d8267a154852f3ba63c06e2b84a2cfd30e9
jskang24 <jskang24@pupils.nlcsjeju.kr>
2023-01-14 00:09:11 +0900

    stats update 2

--- src/pages/Testimonials.js
+++ src/pages/Testimonials.js
@@ -7,22 +7,24 @@ const Testimonials = () => {
   return (
     <div className="Testimonials">
       <Navbar />
-      <div className="Statistics">
-        <span className="Statistics_Title">STATISTICS</span>
-        <div className="Statistics_Stats">
-          <div className="Statistics_Stat">
-            <span className="Statistics_Major">100%</span>
-            <span className="Statistics_Minor">Of users were satisfied</span>
-          </div>
-          <div className="Statistics_Stat">
-            <span className="Statistics_Major">5.0</span>
-            <span className="Statistics_Minor">/5 rating by users</span>
-          </div>
-          <div className="Statistics_Stat">
-            <span className="Statistics_Major">100%</span>
-            <span className="Statistics_Minor">
-              of users found windownet easy to use
-            </span>
+      <div className="Testimonials_Animation">
+        <div className="Statistics">
+          <span className="Statistics_Title">STATISTICS</span>
+          <div className="Statistics_Stats">
+            <div className="Statistics_Stat">
+              <span className="Statistics_Major">100%</span>
+              <span className="Statistics_Minor">Of users were satisfied</span>
+            </div>
+            <div className="Statistics_Stat">
+              <span className="Statistics_Major">5.0</span>
+              <span className="Statistics_Minor">/5 rating by users</span>
+            </div>
+            <div className="Statistics_Stat">
+              <span className="Statistics_Major">100%</span>
+              <span className="Statistics_Minor">
+                of users found windownet easy to use
+              </span>
+            </div>
           </div>
         </div>
       </div>
